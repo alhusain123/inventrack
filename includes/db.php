3a73b2ff2f0ea1d7e37361953e2b8b19edd349c3
Husain <dikcataotan@gmail.com>
2026-05-26 21:15:58 +0800

    Fix database connection

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,9 @@
 <?php
-define('DB_HOST', 'mysql.railway.internal');
+define('DB_HOST', 'zephyr.proxy.rlwy.net');
 define('DB_USER', 'root');
 define('DB_PASS', 'zJxjQnnaXONmHIcZiDYmChsMynbtCsjN');
 define('DB_NAME', 'railway');
-define('DB_PORT', 3306);
+define('DB_PORT', 32725);
 
 function getConnection() {
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
