c938a5b109be5413d71395471a3a69befc886b56
Husain <dikcataotan@gmail.com>
2026-05-26 21:08:54 +0800

    Fix redirect paths

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+session_start();
+if (isset($_SESSION['user_id'])) {
+    header("Location: dashboard.php");
+} else {
+    header("Location: login.php");
+}
+exit();
 ?>
-Something is wrong with the XAMPP installation :-(
